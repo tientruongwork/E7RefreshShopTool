a16250d25ae8e1d4ef9682d73ba2d34aef22a84d
Tien Truong <tientruong.work@gmail.com>
2023-05-03 15:21:40 +0700

    [Tien] fix coordinates scripts

--- scripts/get_pointer_position.py
+++ scripts/get_pointer_position.py
@@ -1,3 +1,3 @@
-import pyautogui
+import win32gui
 
-print("[" + str(pyautogui.position()[0]) + "- app_rect[0]" + ", " + str(pyautogui.position()[1])+ "- app_rect[1]" +"]")
+print("[" + str(win32gui.GetCursorPos()[0]) + "- app_rect[0]" + ", " + str(win32gui.GetCursorPos()[1])+ "- app_rect[1]" +"]")
